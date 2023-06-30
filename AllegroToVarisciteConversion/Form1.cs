1a056632da9f40c88fff31582bb004a66d8cedd1
rozen <rozenberg.kim@gmail.com>
2023-06-30 16:07:40 +0300

    Finish with the coords

--- AllegroToVarisciteConversion/Form1.cs
+++ AllegroToVarisciteConversion/Form1.cs
@@ -36,13 +36,15 @@ namespace AllegroToVarisciteConversion
             {
                 this.full_patch_Coords = openFileDialog1.FileName;
             }
+
+            this.coords = InitElementCoords();
         }
         
         private List<MyDictionary> InitElementCoords()
         {
             List<MyDictionary> coords = new List<MyDictionary>();
 
-            MyDictionary temp;
+            MyDictionary temp = null;
 
             using (var reader = new StreamReader(this.full_patch_Coords))
             {
@@ -51,32 +53,47 @@ namespace AllegroToVarisciteConversion
                     var line = reader.ReadLine().Split(' ');
                     if (HasName(line))
                     {
-                        string name = line[12];
-                        if(!IsNameInList(name, coords))
-                        {
-                            temp = new MyDictionary(name);
-                            if (HasCoords(line))
-                            {
-                                var t1 = string.Concat(line[3].Where(Char.IsDigit));
-                                t1 = t1.Substring(0, t1.Length - 2);
-                                var t2 = string.Concat(line[4].Where(Char.IsDigit));
-                                t2 = t2.Substring(0, t2.Length - 2);
-                                var t3 = string.Concat(line[6].Where(Char.IsDigit));
-                                t3 = t3.Substring(0, t3.Length - 2);
-                                var t4 = string.Concat(line[7].Where(Char.IsDigit));
-                                t4 = t4.Substring(0, t4.Length - 2);
+                        coords.Add(temp);
+                        temp = new MyDictionary(line[12]);
+                    }
+                    if (EndOfFile(line))
+                    {
+                        coords.Add(temp);
+                    }
 
-                                //Console.WriteLine($"({t1},{t2}) ({t3},{t4})");
-                                temp.AddValue(Convert.ToInt32(t1), Convert.ToInt32(t2));
-                                temp.AddValue(Convert.ToInt32(t3), Convert.ToInt32(t4));
-                            }
-                        }
+                    if (HasCoords(line))
+                    {
+                        var t1 = string.Concat(line[3].Where(Char.IsDigit));
+                        t1 = t1.Substring(0, t1.Length - 2);
+                        var t2 = string.Concat(line[4].Where(Char.IsDigit));
+                        t2 = t2.Substring(0, t2.Length - 2);
+                        var t3 = string.Concat(line[6].Where(Char.IsDigit));
+                        t3 = t3.Substring(0, t3.Length - 2);
+                        var t4 = string.Concat(line[7].Where(Char.IsDigit));
+                        t4 = t4.Substring(0, t4.Length - 2);
+
+                        //Console.WriteLine($"({t1},{t2}) ({t3},{t4})");
+                        temp.AddValue(int.Parse(t1), int.Parse(t2));
+                        //temp.AddValue(int.Parse(t3), int.Parse(t4));
+
                     }
                 }
             }
-
+            coords.RemoveAt(0);
             return coords;
         }
+        public static bool EndOfFile(string[] line)
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i].Equals("~end-of-file~"))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        /*
         public static List<string> ConverToList(string[] arr)
         {
             List<string> strings = new List<string>();
@@ -89,7 +106,20 @@ namespace AllegroToVarisciteConversion
             
 
             return strings;
-        }
+        }*/
+        /*
+        public static bool EndOfItem(string[] line)
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i].Equals("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }*/
+
         public static bool HasCoords(string[] line)
         {
             for (int i = 0; i < line.Length; i++)
@@ -108,7 +138,7 @@ namespace AllegroToVarisciteConversion
             }
             return false;
         }
-
+        /*
         private bool IsNameInList(string name, List<MyDictionary> coords)
         {
             for (int i = 0; i < coords.Count; i++)
@@ -119,11 +149,11 @@ namespace AllegroToVarisciteConversion
                 }
             }
             return false;
-        }
+        }*/
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            this.coords = InitElementCoords();
+            
 
             MessageBox.Show("File Saved");
         }
