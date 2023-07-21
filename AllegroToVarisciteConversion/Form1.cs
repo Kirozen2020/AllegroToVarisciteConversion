e0c5390138acffde7ac4aef6afa54cecb529fb9c
rozen <rozenberg.kim@gmail.com>
2023-07-21 13:27:04 +0300

    update name of the form to the correct name

--- AllegroToVarisciteConversion/Form1.cs
+++ AllegroToVarisciteConversion/Form1.cs
@@ -22,7 +22,14 @@ namespace AllegroToVarisciteConversion
             InitializeComponent();
 
             pbSketch.Dock = DockStyle.Fill;
-            string commitId = GetLastPatchID();
+            if (IsThereNotCommitedChanges())
+            {
+                this.Text = "AllegroRoVarisciteConversion " + GetLastPatchID() + " -dirty";
+            }
+            else
+            {
+                this.Text = "AllegroRoVarisciteConversion " + GetLastPatchID();
+            }
             this.logTextGlobal.AppendLine("Program Start");
             infoToolStripMenuItem.CheckState = CheckState.Checked;
         }
@@ -91,6 +98,10 @@ namespace AllegroToVarisciteConversion
         /// </summary>
         private StringBuilder logTextGlobal = new StringBuilder();
 
+        /// <summary>
+        /// Gets the last patch identifier.
+        /// </summary>
+        /// <returns></returns>
         private string GetLastPatchID()
         {
             string gitPath = @"C:\Program Files\Git\cmd\git.exe"; // Replace with the path to the git executable if it's not in the system's PATH environment variable.
@@ -117,12 +128,50 @@ namespace AllegroToVarisciteConversion
                 // Wait for the process to finish
                 process.WaitForExit();
 
-                //Console.WriteLine($"The last commit ID is: {commitId}");
-                MessageBox.Show(commitId);
+                //MessageBox.Show(commitId);
                 return commitId;
             }
         }
+        /// <summary>
+        /// Determines whether [is there not commited changes].
+        /// </summary>
+        /// <returns>
+        ///   <c>true</c> if [is there not commited changes]; otherwise, <c>false</c>.
+        /// </returns>
+        private bool IsThereNotCommitedChanges()
+        {
+            string gitPath = "git"; // Replace with the path to the git executable if it's not in the system's PATH environment variable.
 
+            // Set up the process information
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = gitPath,
+                Arguments = "status --porcelain",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            // Create and start the process
+            using (Process process = new Process())
+            {
+                process.StartInfo = startInfo;
+                process.Start();
+
+                // Read the output of the git status command
+                string output = process.StandardOutput.ReadToEnd();
+
+                // Wait for the process to finish
+                process.WaitForExit();
+
+                // Check if there are any changes to be committed
+                bool hasChanges = !string.IsNullOrWhiteSpace(output);
+
+                //Console.WriteLine($"Are there any changes to be committed? {hasChanges}");
+                //MessageBox.Show(hasChanges.ToString());
+                return hasChanges;
+            }
+        }
 
         /// <summary>
         /// Initializes the tabel.
