bec62d6ecb9e99b1574ab2f1fdc56bbdd899e3b4
rozen <rozenberg.kim@gmail.com>
2023-08-02 12:35:58 +0300

    update MyDictionary class

--- AllegroToVarisciteConversion/MyDictionary.cs
+++ AllegroToVarisciteConversion/MyDictionary.cs
@@ -9,19 +9,48 @@ namespace AllegroToVarisciteConversion
 {
     internal class MyDictionary
     {
+        /// <summary>
+        /// Gets the key.
+        /// </summary>
+        /// <value>
+        /// The key.
+        /// </value>
         public string Key { get; }
-        public List<Point> Value { get; }
-
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        public List<Point3D> Value { get; }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyDictionary"/> class.
+        /// </summary>
+        /// <param name="key">The key.</param>
         public MyDictionary(string key) {
             this.Key = key;
-            this.Value = new List<Point>();
+            this.Value = new List<Point3D>();
         }
-
+        /// <summary>
+        /// Adds the value.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
         public void AddValue(double x, double y)
         {
-            Point p = new Point((int)x, (int)y);
+            Point3D p = new Point3D((int)x, (int)y);
             this.Value.Add(p);
         }
-
+        /// <summary>
+        /// Adds the value.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <param name="z">The z.</param>
+        public void AddValue(double x, double y, double z)
+        {
+            Point3D p = new Point3D((int)x, (int)y, (int)z);
+            this.Value.Add(p);
+        }
     }
 }
