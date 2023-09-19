71bb2fa3fb3aeb239e828ec1f01cfdd7aa5c33dc
rozen <rozenberg.kim@gmail.com>
2023-09-19 15:42:05 +0300

    add LogManager file

---
+++ AllegroToVarisciteConversion/LogManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllegroToVarisciteConversion
+{
+    internal class LogManager
+    {
+        /// <summary>
+        /// The log text
+        /// </summary>
+        private StringBuilder logTextInfoPlacementCoordinates = new StringBuilder();
+        /// <summary>
+        /// The log text information placement report
+        /// </summary>
+        private StringBuilder logTextInfoPlacementReport = new StringBuilder();
+        /// <summary>
+        /// The log text debug
+        /// </summary>
+        private StringBuilder logTextDebugPlacementCoordinates = new StringBuilder();
+        /// <summary>
+        /// The log text debug placement report
+        /// </summary>
+        private StringBuilder logTextDebugPlacementReport = new StringBuilder();
+        /// <summary>
+        /// The log text error
+        /// </summary>
+        private StringBuilder logTextErrorPlacementCoordinates = new StringBuilder();
+        /// <summary>
+        /// The log text error placement report
+        /// </summary>
+        private StringBuilder logTextErrorPlacementReport = new StringBuilder();
+
+        /*
+        0 - Info 
+        1 - Debug
+        2 - Error
+         */
+    }
+}
