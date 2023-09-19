5d370c5a8e8fc88ac30dd1e23669776f15916f84
rozen <rozenberg.kim@gmail.com>
2023-09-19 23:16:17 +0300

    fill LogManager file

--- AllegroToVarisciteConversion/LogManager.cs
+++ AllegroToVarisciteConversion/LogManager.cs
@@ -11,32 +11,108 @@ namespace AllegroToVarisciteConversion
         /// <summary>
         /// The log text
         /// </summary>
-        private StringBuilder logTextInfoPlacementCoordinates = new StringBuilder();
+        private StringBuilder logTextInfoPlacementCoordinates;
         /// <summary>
         /// The log text information placement report
         /// </summary>
-        private StringBuilder logTextInfoPlacementReport = new StringBuilder();
+        private StringBuilder logTextInfoPlacementReport;
         /// <summary>
         /// The log text debug
         /// </summary>
-        private StringBuilder logTextDebugPlacementCoordinates = new StringBuilder();
+        private StringBuilder logTextDebugPlacementCoordinates;
         /// <summary>
         /// The log text debug placement report
         /// </summary>
-        private StringBuilder logTextDebugPlacementReport = new StringBuilder();
+        private StringBuilder logTextDebugPlacementReport;
         /// <summary>
         /// The log text error
         /// </summary>
-        private StringBuilder logTextErrorPlacementCoordinates = new StringBuilder();
+        private StringBuilder logTextErrorPlacementCoordinates;
         /// <summary>
         /// The log text error placement report
         /// </summary>
-        private StringBuilder logTextErrorPlacementReport = new StringBuilder();
+        private StringBuilder logTextErrorPlacementReport;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogManager"/> class.
+        /// </summary>
+        public LogManager()
+        {
+            ClearCoordsLog();
+            ClearPlacementLog();
+        }
 
         /*
+        Menu :
+
         0 - Info 
         1 - Debug
         2 - Error
          */
+
+        /// <summary>
+        /// Adds the comment.
+        /// </summary>
+        /// <param name="comment">The comment.</param>
+        /// <param name="id">The identifier.</param>
+        /// <param name="categoty">The categoty.</param>
+        public void AddComment(string comment, List<int> id, string categoty)
+        {
+            for (int i = 0; i < id.Count; i++)
+            {
+                switch (id[i])
+                {
+                    case 0:
+                        if(categoty == "coords")
+                        {
+                            logTextInfoPlacementCoordinates.Append(comment);
+                        }
+                        else if(categoty == "placement")
+                        {
+                            logTextInfoPlacementCoordinates.Append(comment);
+                        }
+                        break;
+                    case 1:
+                        if (categoty == "coords")
+                        {
+                            logTextDebugPlacementCoordinates.Append(comment);
+                        }
+                        else if (categoty == "placement")
+                        {
+                            logTextDebugPlacementReport.Append(comment);
+                        }
+                        break;
+                    case 2:
+                        if (categoty == "coords")
+                        {
+                            logTextErrorPlacementCoordinates.Append(comment);
+                        }
+                        else if (categoty == "placement")
+                        {
+                            logTextErrorPlacementReport.Append(comment);
+                        }
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Clears the placement log.
+        /// </summary>
+        public void ClearPlacementLog()
+        {
+            this.logTextInfoPlacementReport = new StringBuilder();
+            this.logTextDebugPlacementReport = new StringBuilder();
+            this.logTextErrorPlacementReport = new StringBuilder();
+        }
+        /// <summary>
+        /// Clears the coords log.
+        /// </summary>
+        public void ClearCoordsLog()
+        {
+            this.logTextInfoPlacementCoordinates = new StringBuilder();
+            this.logTextErrorPlacementCoordinates = new StringBuilder();
+            this.logTextDebugPlacementCoordinates = new StringBuilder();
+        }
     }
 }
